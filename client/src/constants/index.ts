b428c34fbce1f37c7c987d30334d29ba4b8a1eed
Luke Olsen <lukeaolsen@gmail.com>
2022-01-17 18:48:26 -0500

    available years

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -4,3 +4,5 @@ export const LOADING = "LOADING";
 export const DONE_LOADING = "DONE_LOADING";
 export const LOAD_CACHE = "LOAD_CACHE";
 export const SET_CACHE = "SET_CACHE";
+
+export const AVAILABLE_YEARS = [2021, 2020, 2019];
