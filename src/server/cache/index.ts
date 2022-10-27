4747a41dad5727b94536ed7d04196f62af2edcb3
Luke Olsen <lukeaolsen@gmail.com>
2022-10-27 15:37:56 -0400

    cache create

---
+++ src/server/cache/index.ts
@@ -0,0 +1,24 @@
+import { prisma } from "../server";
+
+export const cache: { teams: Cache.Teams[] | null } = { teams: null };
+
+export const creatCache = async () => {
+  cache.teams = await prisma.teams.findMany({
+    select: {
+      id: true,
+      school: true,
+      mascot: true,
+      abbreviation: true,
+      conference: true,
+      division: true,
+      color: true,
+      logos_0: true,
+      logos_1: true,
+    },
+    orderBy: {
+      conference: "asc",
+    },
+  });
+  console.log("cache loaded");
+  return cache;
+};
